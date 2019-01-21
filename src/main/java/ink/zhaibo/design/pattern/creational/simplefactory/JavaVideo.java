3508e398cb4d2ae46bd1f18628ecf6be0fb8eb02
zhaibo <729963917@qq.com>
2019-01-21 10:37:48 +0800

    简单工厂.

---
+++ src/main/java/ink/zhaibo/design/pattern/creational/simplefactory/JavaVideo.java
@@ -0,0 +1,11 @@
+package ink.zhaibo.design.pattern.creational.simplefactory;
+
+/**
+ * Created by geely
+ */
+public class JavaVideo extends Video {
+    @Override
+    public void produce() {
+        System.out.println("录制Java课程视频");
+    }
+}
