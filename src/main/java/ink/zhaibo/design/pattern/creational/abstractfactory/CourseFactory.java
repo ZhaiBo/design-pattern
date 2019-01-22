028419f055933f1827aa933c9108a5242fd47c72
zhaibo <729963917@qq.com>
2019-01-22 08:42:52 +0800

    抽象工厂.

---
+++ src/main/java/ink/zhaibo/design/pattern/creational/abstractfactory/CourseFactory.java
@@ -0,0 +1,11 @@
+package ink.zhaibo.design.pattern.creational.abstractfactory;
+
+
+/**
+ * Created by geely
+ */
+public interface CourseFactory {
+    Video getVideo();
+    Article getArticle();
+
+}
---
+++ src/main/java/ink/zhaibo/design/pattern/creational/abstractfactory/Video.java
@@ -0,0 +1,9 @@
+package ink.zhaibo.design.pattern.creational.abstractfactory;
+
+/**
+ * Created by geely
+ */
+public abstract class Video {
+    public abstract void produce();
+
+}
