8da1adb92d20adea012b7467c333a0367f0d811c
zhaibo <729963917@qq.com>
2019-01-26 15:52:52 +0800

    迭代器模式.

---
+++ src/main/java/ink/zhaibo/design/pattern/behavioral/iterator/Course.java
@@ -0,0 +1,17 @@
+package ink.zhaibo.design.pattern.behavioral.iterator;
+
+/**
+ * Created by geely.
+ */
+public class Course {
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/ink/zhaibo/design/pattern/behavioral/iterator/CourseIterator.java
@@ -0,0 +1,10 @@
+package ink.zhaibo.design.pattern.behavioral.iterator;
+
+/**
+ * Created by geely.
+ */
+public interface CourseIterator {
+    Course nextCourse();
+    boolean isLastCourse();
+
+}
