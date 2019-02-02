84d4d643ab7bd825d4e4c4603c2fed16bf37f3b3
zhaibo <729963917@qq.com>
2019-02-02 13:23:15 +0800

    代理模式.

---
+++ src/main/java/ink/zhaibo/design/pattern/structural/proxy/IOrderDao.java
@@ -0,0 +1,9 @@
+package ink.zhaibo.design.pattern.structural.proxy;
+
+/**
+ * Created by geely
+ */
+public interface IOrderDao {
+    int insert(Order order);
+
+}
---
+++ src/main/java/ink/zhaibo/design/pattern/structural/proxy/IOrderService.java
@@ -0,0 +1,8 @@
+package ink.zhaibo.design.pattern.structural.proxy;
+
+/**
+ * Created by geely
+ */
+public interface IOrderService {
+    int saveOrder(Order order);
+}
