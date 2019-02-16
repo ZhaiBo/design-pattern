4d0e95d65a41dc3f662cd98e12c2db0e2eb7e8ec
zhaibo <729963917@qq.com>
2019-02-16 08:48:50 +0800

    装饰者模式.

---
+++ src/main/java/ink/zhaibo/design/pattern/structural/decorator/v2/ABattercake.java
@@ -0,0 +1,10 @@
+package ink.zhaibo.design.pattern.structural.decorator.v2;
+
+/**
+ * Created by geely
+ */
+public abstract class ABattercake {
+    protected abstract String getDesc();
+    protected abstract int cost();
+
+}
